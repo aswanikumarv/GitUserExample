636f042f8cb3bad83bf867416fc544d23a3c5873
aswanikumarv <vak556@gmail.com>
2015-08-24 19:35:14 +0530

    Commit 1

---
+++ GitUserExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitUserExample
+//
+//  Created by Admin on 24/08/15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitUserExample/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  GitUserExample
+//
+//  Created by Admin on 24/08/15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)clickTheButton:(id)sender {
+    
+    
+    
+     NSLog(@"This is a Git tutorial");
+    
+}
+
+@end
